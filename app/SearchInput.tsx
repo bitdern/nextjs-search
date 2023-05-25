61edc03737f731c49460400b1a65620c17ba555b
aidan.newell <acnewell09@gmail.com>
2023-05-25 08:23:08 -0500

    working through ts linting errors

--- app/SearchInput.tsx
+++ app/SearchInput.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import { useState } from "react";
+
 const SearchInput = () => {
-  <input className="" />;
+  const [searchQuery, setSearchQuery] = useState("");
+
+  return (
+    <input
+      value={searchQuery}
+      onChange={(event) => setSearchQuery(event.target.value)}
+      className="px-5 py-1 w-2/3 sm:px-5 sm:py-3 flex-1 text-zinc-200 bg-zinc-800 focus:bg-black rounded-full focus:outline-none focus:ring-[1px] focus:ring-green-700 placeholder:text-zinc-400"
+      placeholder="What are you looking for?"
+    />
+  );
 };
 
 export default SearchInput;
