3f27302345b7941e27829016a9a95ebb01d635eb
aidan.newell <acnewell09@gmail.com>
2023-05-27 09:20:25 -0500

    working on GET request fucntionality

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,12 +1,27 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import useSWR from "swr";
+
+const fetchPosts = async (url: string) => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch posts");
+  }
+
+  return response.json();
+};
 
 const SearchPage = () => {
   const search = useSearchParams();
   const searchQuery = search ? search.get("q") : null;
-
   const encodedSearchQuery = encodeURI(searchQuery || "");
+
+  const { data, isLoading } = useSWR(
+    `/api/search?q=${encodedSearchQuery}`,
+    fetchPosts
+  );
 
   console.log("SEARCH PARAMS", encodedSearchQuery);
 
